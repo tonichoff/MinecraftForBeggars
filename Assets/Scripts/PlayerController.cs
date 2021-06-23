525fbdfdab0a1ea3bf5f228d782ef115ec8cd1aa
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-06-23 18:44:48 +1000

    Fix camera nug. Returrn multijumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,13 +44,13 @@ public class PlayerController : MonoBehaviour
         var x = Input.GetAxis("Horizontal");
         var z = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space) && body.detectCollisions) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             body.AddForce(Vector3.up * jumpPower);
         }
 
         var transformVector = body.transform.localToWorldMatrix.MultiplyVector(new Vector3(x, 0, z));
         var deltaPosition = transformVector * speed * Time.deltaTime;
-        body.transform.position += deltaPosition;
+        body.MovePosition(body.transform.position + deltaPosition);
     }
 
     private void UpdateDestroyingVoxel()
@@ -71,7 +71,7 @@ public class PlayerController : MonoBehaviour
 	{
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
         if (Physics.Raycast(ray, out var hit)) {
-            if (hit.collider.name == "Cube") {
+            if (hit.collider.name.Contains("Cube")) {
                 var wg = GameObject.Find("WorldGenerator").GetComponent<WorldGenerator>();
                 wg.CreateVoxel(wg.GrassVoxel, hit.transform.position + hit.normal);
             }
