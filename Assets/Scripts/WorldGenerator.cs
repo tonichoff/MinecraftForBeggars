c8e62715c941abb9eb7c90d0e94fcfaec85cfb81
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-06-24 18:14:33 +1000

    Generate mountains

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using UnityEngine;
 
 public enum VoxelType
@@ -10,6 +12,8 @@ public enum VoxelType
 
 public class WorldGenerator : MonoBehaviour
 {
+    private readonly int voxelsPerFrame = 5000;
+
     public int WorldSize;
     public int WorldHeight;
 
@@ -26,15 +30,65 @@ public class WorldGenerator : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        StartCoroutine(GenerateWorld());
+    }
+
+    private IEnumerator GenerateWorld()
+	{
+        var map = new int[WorldSize, WorldSize];
+        for (int x = 0; x < WorldSize; ++x) {
+            for (int z = 0; z < WorldSize; ++z) {
+                map[x, z] = Random.Range(1, WorldHeight);
+            }
+        }
+
+        for (int i = 0; i < 25; ++i) {
+            int x = Random.Range(1, WorldSize);
+            int y = Random.Range(1, WorldSize);
+            map[x, y] = WorldHeight * 30;
+        }
+
+        for (int k = 0; k < 5; ++k) {
+            var smoothMap = new int[WorldSize, WorldSize];
+            for (int x = 0; x < WorldSize; ++x) {
+                for (int z = 0; z < WorldSize; ++z) {
+                    var sum = 0;
+                    var counter = 0;
+                    for (int dx = -1; dx <= 1; ++dx) {
+                        for (int dz = -1; dz <= 1; ++dz) {
+                            var nx = x + dx;
+                            var nz = z + dz;
+                            if (nx >= 0 && nx < WorldSize && nz >= 0 && nz < WorldSize) {
+                                sum += map[nx, nz];
+                                counter++;
+                            }
+                        }
+                    }
+                    smoothMap[x, z] = sum / counter;
+                }
+            }
+            for (int x = 0; x < WorldSize; ++x) {
+                for (int z = 0; z < WorldSize; ++z) {
+                    map[x, z] = smoothMap[x, z];
+                }
+            }
+        }
+
+        var voxelsInFrameCounter = 0;
         for (int x = 0; x < WorldSize; ++x) {
             for (int z = 0; z < WorldSize; ++z) {
                 CreateVoxel(BedrockVoxel, new Vector3(x, 0, z));
-                var height = Random.Range(1, WorldHeight);
+                var height = map[x, z];
                 for (int y = 1; y < height; ++y) {
                     CreateVoxel(GrassVoxel, new Vector3(x, y, z));
-				}
-			}
-		}
+                    ++voxelsInFrameCounter;
+                    if (voxelsInFrameCounter >= voxelsPerFrame) {
+                        voxelsInFrameCounter = 0;
+                        yield return new WaitForEndOfFrame();
+					}
+                }
+            }
+        }
     }
 
     // Update is called once per frame
